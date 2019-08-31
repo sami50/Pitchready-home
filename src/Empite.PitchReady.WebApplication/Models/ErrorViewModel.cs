9084c5548d9f3472b0c3077efa85b1ca20ec3175
Ranjan tharmkaulasingam <ranjan@empite.com>
2019-09-01 09:03:04 +1000

    initi

---
+++ src/Empite.PitchReady.WebApplication/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Empite.PitchReady.WebApplication.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
